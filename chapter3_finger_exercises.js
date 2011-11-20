221d4c31f2179943535583c9104032b6b234c96c
Paul Curry <pcurry@gmail.com>
2011-11-19 20:00:44 -0500

    More typing progress

--- chapter3_finger_exercises.js
+++ chapter3_finger_exercises.js
@@ -9,7 +9,32 @@ var flight = {
     airline: "Oceanic",
     number: 815,
     departure: {
-	IATA
-    
-}
+	IATA: "SYD",
+	time: "2004-09-22 14:55",
+	city: "Sydney"
+    },
+    arrival: {
+	IATA: "LAX",
+	ttime: "2004-09-10:42",
+	city: "Los Angeles"
+    }
+};
 
+document.writeln(stooge["first-name"]);
+document.writeln(flight.departure.IATTA);
+
+document.writeln(stooge["middle-name"]);
+document.writeln(flight.status);
+document.writeln(stooge["first-name"]);
+
+var middle = stooge["middle-name"] || "(none)";
+document.writeln(middle);
+
+var status = flight.status || "unknown";
+document.writeln(status);
+
+flight.equipment;
+// flight.equipment.model // Throws a TypeError
+flight.equipment && flight.equipment.model;
+
+stooge["first-name"] = 'Jerome';
